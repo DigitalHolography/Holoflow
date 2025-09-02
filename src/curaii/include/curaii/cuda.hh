7caf7dad08179af52d688d257c0c5c5b4c5e27e6
Jules Guillou <guilloujules@gmail.com>
2025-09-02 16:20:28 +0200

    feat(curaii): initial commit for curaii. Added minimal cuda wrappers

---
+++ src/curaii/include/curaii/cuda.hh
@@ -0,0 +1,110 @@
+// Copyright 2025 Digital Holography Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#pragma once
+
+#include <cuda_runtime.h>
+#include <memory>
+#include <spdlog/spdlog.h>
+#include <stdexcept>
+
+namespace curaii::detail {
+
+void log_cuda_failure(spdlog::level::level_enum lvl, cudaError_t code,
+                      const char *expr, const char *file, int line);
+
+} // namespace curaii::detail
+
+#define CUDA_CHECK(expr)                                                       \
+  do {                                                                         \
+    cudaError_t err__ = (expr);                                                \
+    if (err__ != cudaSuccess) {                                                \
+      ::curaii::detail::log_cuda_failure(spdlog::level::warn, err__, #expr,    \
+                                         __FILE__, __LINE__);                  \
+      throw ::curaii::CudaError(err__, #expr, __FILE__, __LINE__);             \
+    }                                                                          \
+  } while (false)
+
+#define CUDA_CHECK_NT(expr)                                                    \
+  do {                                                                         \
+    cudaError_t err__ = (expr);                                                \
+    if (err__ != cudaSuccess) {                                                \
+      ::curaii::detail::log_cuda_failure(spdlog::level::critical, err__,       \
+                                         #expr, __FILE__, __LINE__);           \
+      std::abort();                                                            \
+    }                                                                          \
+  } while (false)
+
+namespace curaii {
+
+class CudaError : public std::runtime_error {
+public:
+  explicit CudaError(cudaError_t code, const char *what, const char *file,
+                     int line);
+
+  [[nodiscard]] cudaError_t code() const noexcept;
+
+private:
+  static std::string make_message(cudaError_t code, const char *what,
+                                  const char *file, int line);
+
+  cudaError_t code_;
+};
+
+struct HostDeleter {
+  void operator()(void *ptr) const noexcept;
+};
+
+struct DeviceDeleter {
+  void operator()(void *ptr) const noexcept;
+};
+
+template <typename T> using unique_host_ptr = std::unique_ptr<T, HostDeleter>;
+
+template <typename T>
+[[nodiscard]] unique_host_ptr<T> make_unique_host_ptr(size_t count);
+
+template <typename T>
+using unique_device_ptr = std::unique_ptr<T, DeviceDeleter>;
+
+template <typename T>
+[[nodiscard]] unique_device_ptr<T>
+make_unique_device_ptr(size_t count, cudaStream_t stream = 0);
+
+class CudaStream {
+public:
+  explicit CudaStream(unsigned flags = cudaStreamDefault, int priority = 0);
+  ~CudaStream() noexcept;
+
+  CudaStream(const CudaStream &)            = delete;
+  CudaStream &operator=(const CudaStream &) = delete;
+
+  CudaStream(CudaStream &&other) noexcept;
+  CudaStream &operator=(CudaStream &&other) noexcept;
+
+  cudaStream_t get() const noexcept;
+  cudaStream_t release() noexcept;
+  void         reset(cudaStream_t s = nullptr) noexcept;
+  explicit     operator bool() const noexcept;
+
+private:
+  cudaStream_t stream_{nullptr};
+};
+
+} // namespace curaii
+
+#define CUURAII_CUDA_HXX_INCLUDED
+#include "curaii/cuda.hxx"
+#undef CUURAII_CUDA_HXX_INCLUDED
---
+++ src/curaii/src/cuda/cuda.cc
@@ -0,0 +1,100 @@
+// Copyright 2025 Digital Holography Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "curaii/cuda.hh"
+
+#include "logger.hh"
+
+namespace curaii {
+
+CudaError::CudaError(cudaError_t code, const char *what, const char *file,
+                     int line)
+    : std::runtime_error(CudaError::make_message(code, what, file, line)),
+      code_(code) {}
+
+std::string CudaError::make_message(cudaError_t code, const char *what,
+                                    const char *file, int line) {
+  std::ostringstream os;
+  os << "CUDA error: " << cudaGetErrorString(code) << " (" << code << ")\n"
+     << "  expression : " << what << '\n'
+     << "  location   : " << file << ':' << line;
+  return os.str();
+}
+
+cudaError_t CudaError::code() const noexcept { return code_; }
+
+void HostDeleter::operator()(void *ptr) const noexcept {
+  if (ptr) {
+    CUDA_CHECK_NT(cudaFreeHost(ptr));
+  }
+}
+
+void DeviceDeleter::operator()(void *ptr) const noexcept {
+  if (ptr) {
+    CUDA_CHECK_NT(cudaFree(ptr));
+  }
+}
+
+CudaStream::CudaStream(unsigned flags, int priority) {
+  CUDA_CHECK(cudaStreamCreateWithPriority(&stream_, flags, priority));
+}
+
+CudaStream::~CudaStream() noexcept {
+  if (stream_) {
+    CUDA_CHECK_NT(cudaStreamDestroy(stream_));
+  }
+}
+
+CudaStream::CudaStream(CudaStream &&other) noexcept : stream_(other.stream_) {
+  other.stream_ = nullptr;
+}
+
+CudaStream &CudaStream::operator=(CudaStream &&other) noexcept {
+  if (this != &other) {
+    reset();
+    stream_       = other.stream_;
+    other.stream_ = nullptr;
+  }
+  return *this;
+}
+
+cudaStream_t CudaStream::get() const noexcept { return stream_; }
+
+cudaStream_t CudaStream::release() noexcept {
+  auto tmp = stream_;
+  stream_  = nullptr;
+  return tmp;
+}
+
+void CudaStream::reset(cudaStream_t s) noexcept {
+  if (stream_) {
+    CUDA_CHECK_NT(cudaStreamDestroy(stream_));
+  }
+  stream_ = s;
+}
+
+CudaStream::operator bool() const noexcept { return stream_ != nullptr; }
+
+} // namespace curaii
+
+namespace curaii::detail {
+
+void log_cuda_failure(spdlog::level::level_enum lvl, cudaError_t code,
+                      const char *expr, const char *file, int line) {
+  logger()->log(lvl, "CUDA error {} ({}): \"{}\"  [{}:{}]",
+                cudaGetErrorString(code), static_cast<int>(code), expr, file,
+                line);
+}
+
+} // namespace curaii::detail
