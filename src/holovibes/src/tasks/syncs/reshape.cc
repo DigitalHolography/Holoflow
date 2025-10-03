0752b66ca850e0039eb6c99617f176cc8aabab4d
Jules Guillou <guilloujules@gmail.com>
2025-10-03 11:02:03 +0200

    refactor(holovibes): splitted tasks in source sinks syncs and asyncs

--- src/holovibes/src/tasks/syncs/reshape.cc
+++ src/holovibes/src/tasks/syncs/reshape.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "tasks/reshape.hh"
+#include "reshape.hh"
 
 #include "bug.hh"
 #include "curaii/cuda.hh"
