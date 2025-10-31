e68591c3fc6a974a7cfc6be6e1d96aa2d6e01eac
Cyril Drexler <dcyrilmc@gmail.com>
2025-10-31 16:17:06 +0100

    fix(holovibes): removed hard coded footer data

--- src/holovibes/src/settings_loader.cc
+++ src/holovibes/src/settings_loader.cc
@@ -25,16 +25,16 @@ nlohmann::json settings_to_old_json(const Settings &settings) {
   j["compute_settings"]["advanced"]["buffer_size"]["output"] = settings.gpu_out_size;
   j["compute_settings"]["advanced"]["buffer_size"]["record"] = settings.cpu_rec_size;
   // FIXME: buffer_size.file - no mapping found
-  j["compute_settings"]["advanced"]["buffer_size"]["file"] = 1024;
+  //   j["compute_settings"]["advanced"]["buffer_size"]["file"] = 1024;
   // FIXME: buffer_size.time_transformation_cuts - no mapping found
-  j["compute_settings"]["advanced"]["buffer_size"]["time_transformation_cuts"] = 512;
+  //   j["compute_settings"]["advanced"]["buffer_size"]["time_transformation_cuts"] = 512;
 
   // FIXME: contrast.frame_index_offset - no mapping found
-  j["compute_settings"]["advanced"]["contrast"]["frame_index_offset"] = 2;
+  //   j["compute_settings"]["advanced"]["contrast"]["frame_index_offset"] = 2;
   // FIXME: contrast.lower - no mapping found
-  j["compute_settings"]["advanced"]["contrast"]["lower"] = 0.02;
+  //   j["compute_settings"]["advanced"]["contrast"]["lower"] = 0.02;
   // FIXME: contrast.upper - no mapping found
-  j["compute_settings"]["advanced"]["contrast"]["upper"] = 99.98;
+  //   j["compute_settings"]["advanced"]["contrast"]["upper"] = 99.98;
 
   // FIXME: make sure it s good
   j["compute_settings"]["advanced"]["filter2d_smooth"]["high"] = settings.filter_smooth_outer;
@@ -43,13 +43,13 @@ nlohmann::json settings_to_old_json(const Settings &settings) {
   // FIXME: nb_frames_to_record - closest is recording_count but context differs
   j["compute_settings"]["advanced"]["nb_frames_to_record"] = settings.recording_count;
   // FIXME: raw_bitshift - no mapping found
-  j["compute_settings"]["advanced"]["raw_bitshift"] = 0;
+  //   j["compute_settings"]["advanced"]["raw_bitshift"] = 0;
   // FIXME: renorm_constant - no mapping found
-  j["compute_settings"]["advanced"]["renorm_constant"] = 5;
+  //   j["compute_settings"]["advanced"]["renorm_constant"] = 5;
 
   // FIXME: entire color_composite_image section - no mapping found
-  j["compute_settings"]["color_composite_image"]["auto_weight"] = false;
-  j["compute_settings"]["color_composite_image"]["mode"]        = "RGB";
+  //   j["compute_settings"]["color_composite_image"]["auto_weight"] = false;
+  //   j["compute_settings"]["color_composite_image"]["mode"]        = "RGB";
   // ... (HSV/RGB subsections omitted for brevity, all FIXMEs)
 
   // Image rendering section
@@ -73,7 +73,7 @@ nlohmann::json settings_to_old_json(const Settings &settings) {
   j["compute_settings"]["image_rendering"]["image_mode"] = image_mode;
 
   // FIXME: input_filter.type - no mapping found
-  j["compute_settings"]["image_rendering"]["input_filter"]["type"] = "None";
+  //   j["compute_settings"]["image_rendering"]["input_filter"]["type"] = "None";
 
   j["compute_settings"]["image_rendering"]["lambda"]               = settings.spacial_lambda;
   j["compute_settings"]["image_rendering"]["propagation_distance"] = settings.spacial_z;
@@ -108,58 +108,58 @@ nlohmann::json settings_to_old_json(const Settings &settings) {
   j["compute_settings"]["image_rendering"]["time_transformation_size"]   = settings.time_window;
   j["compute_settings"]["image_rendering"]["time_transformation_stride"] = settings.time_stride;
 
-  j["compute_settings"]["version"] = "v1";
+  //   j["compute_settings"]["version"] = "v1";
 
   // View section
   j["compute_settings"]["view"]["fft_shift"] = settings.pp_fft_shift;
   // FIXME: image_type - no mapping found
-  j["compute_settings"]["view"]["image_type"] = "MODULUS";
+  //   j["compute_settings"]["view"]["image_type"] = "MODULUS";
 
   j["compute_settings"]["view"]["registration"]["registration_enabled"] = settings.pp_registration;
   j["compute_settings"]["view"]["registration"]["registration_zone"] =
       settings.pp_registration_radius;
 
   // FIXME: renorm - closest might be pp_pctclip but not clear
-  j["compute_settings"]["view"]["renorm"] = true;
+  //   j["compute_settings"]["view"]["renorm"] = true;
 
   // FIXME: reticle section - no mapping found
-  j["compute_settings"]["view"]["reticle"]["display_enabled"] = false;
-  j["compute_settings"]["view"]["reticle"]["scale"]           = 0.5;
+  //   j["compute_settings"]["view"]["reticle"]["display_enabled"] = false;
+  //   j["compute_settings"]["view"]["reticle"]["scale"]           = 0.5;
 
   // Window sections
   // FIXME: filter2d window contrast - different from pp_pctclip
-  j["compute_settings"]["view"]["window"]["filter2d"]["contrast"]["auto_refresh"] = false;
+  //   j["compute_settings"]["view"]["window"]["filter2d"]["contrast"]["auto_refresh"] = false;
   j["compute_settings"]["view"]["window"]["filter2d"]["contrast"]["enabled"] = settings.pp_pctclip;
-  j["compute_settings"]["view"]["window"]["filter2d"]["contrast"]["invert"]  = false;
+  //   j["compute_settings"]["view"]["window"]["filter2d"]["contrast"]["invert"]  = false;
   // FIXME: contrast min/max values
-  j["compute_settings"]["view"]["window"]["filter2d"]["contrast"]["max"] = 65535.0;
-  j["compute_settings"]["view"]["window"]["filter2d"]["contrast"]["min"] = 1.0;
-  j["compute_settings"]["view"]["window"]["filter2d"]["log_enabled"]     = false;
+  //   j["compute_settings"]["view"]["window"]["filter2d"]["contrast"]["max"] = 65535.0;
+  //   j["compute_settings"]["view"]["window"]["filter2d"]["contrast"]["min"] = 1.0;
+  //   j["compute_settings"]["view"]["window"]["filter2d"]["log_enabled"]     = false;
 
   // XY window
-  j["compute_settings"]["view"]["window"]["xy"]["contrast"]["auto_refresh"] = false;
-  j["compute_settings"]["view"]["window"]["xy"]["contrast"]["enabled"]      = settings.pp_pctclip;
-  j["compute_settings"]["view"]["window"]["xy"]["contrast"]["invert"]       = false;
+  //   j["compute_settings"]["view"]["window"]["xy"]["contrast"]["auto_refresh"] = false;
+  j["compute_settings"]["view"]["window"]["xy"]["contrast"]["enabled"] = settings.pp_pctclip;
+  //   j["compute_settings"]["view"]["window"]["xy"]["contrast"]["invert"]       = false;
   // FIXME: xy contrast values - using pctclip but units may differ
   j["compute_settings"]["view"]["window"]["xy"]["contrast"]["max"] = settings.pp_pctclip_upper;
   j["compute_settings"]["view"]["window"]["xy"]["contrast"]["min"] = settings.pp_pctclip_lower;
-  j["compute_settings"]["view"]["window"]["xy"]["enabled"]         = false;
-  j["compute_settings"]["view"]["window"]["xy"]["horizontal_flip"] = false;
-  j["compute_settings"]["view"]["window"]["xy"]["log_enabled"]     = false;
+  //   j["compute_settings"]["view"]["window"]["xy"]["enabled"]         = false;
+  //   j["compute_settings"]["view"]["window"]["xy"]["horizontal_flip"] = false;
+  //   j["compute_settings"]["view"]["window"]["xy"]["log_enabled"]     = false;
   j["compute_settings"]["view"]["window"]["xy"]["output_image_accumulation"] =
       settings.pp_accumulation;
   // FIXME: rotation - no mapping found
-  j["compute_settings"]["view"]["window"]["xy"]["rotation"] = 0.0;
+  //   j["compute_settings"]["view"]["window"]["xy"]["rotation"] = 0.0;
 
   // FIXME: xz and yz windows - no specific mappings
   j["compute_settings"]["view"]["window"]["xz"]["enabled"] = settings.view_3d_cuts;
   j["compute_settings"]["view"]["window"]["yz"]["enabled"] = settings.view_3d_cuts;
 
   // Coordinate ranges
-  j["compute_settings"]["view"]["x"]["start"] = 0; // FIXME: no mapping for x range
-  j["compute_settings"]["view"]["x"]["width"] = 0;
-  j["compute_settings"]["view"]["y"]["start"] = 0; // FIXME: no mapping for y range
-  j["compute_settings"]["view"]["y"]["width"] = 0;
+  //   j["compute_settings"]["view"]["x"]["start"] = 0; // FIXME: no mapping for x range
+  //   j["compute_settings"]["view"]["x"]["width"] = 0;
+  //   j["compute_settings"]["view"]["y"]["start"] = 0; // FIXME: no mapping for y range
+  //   j["compute_settings"]["view"]["y"]["width"] = 0;
   j["compute_settings"]["view"]["z"]["start"] = settings.time_z_begin;
   j["compute_settings"]["view"]["z"]["width"] = settings.time_z_end - settings.time_z_begin;
   // FIXME: z2 - no mapping found
@@ -168,15 +168,15 @@ nlohmann::json settings_to_old_json(const Settings &settings) {
 
   // Info section
   // FIXME: camera_fps - no mapping found
-  j["info"]["camera_fps"] = 37037;
+  //   j["info"]["camera_fps"] = 37037;
   // FIXME: contiguous - no mapping found
   j["info"]["contiguous"] = settings.cpu_rec_size;
   // FIXME: eye_type - no mapping found
-  j["info"]["eye_type"] = "LEFT";
+  //   j["info"]["eye_type"] = "LEFT";
   // FIXME: get the version
-  j["info"]["holovibes_version"] = "0.0.0";
+  //   j["info"]["holovibes_version"] = "0.0.0";
   // FIXME: input_fps - no mapping found
-  j["info"]["input_fps"]        = 37037;
+  //   j["info"]["input_fps"]        = 37037;
   j["info"]["pixel_pitch"]["x"] = settings.spacial_pixel_size;
   j["info"]["pixel_pitch"]["y"] = settings.spacial_pixel_size;
 
