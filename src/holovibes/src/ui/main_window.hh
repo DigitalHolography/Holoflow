362b84bc50d2fb274923aee08804388859cf83da
Cyril Drexler <dcyrilmc@gmail.com>
2025-11-03 13:33:05 +0100

    fix(holovibes): fixed function decleration not present in header

--- src/holovibes/src/ui/main_window.hh
+++ src/holovibes/src/ui/main_window.hh
@@ -80,6 +80,7 @@ private:
   void connect_import_controls();
   void configure_window();
   std::filesystem::path makeRecordingPath(const QString& userText) const;
+  void load_holofile_settings(const nlohmann::json &settings);
   
   // Current state
   bool update_in_progress_ = false;
