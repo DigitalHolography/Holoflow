b90e876b9af619d307af1080bd6db2bc6a8ae8f5
Bastien Desert <bastien.desert@epita.fr>
2026-01-08 14:16:21 +0100

    fix: installer

--- cmake/CPackConfig.cmake
+++ cmake/CPackConfig.cmake
@@ -14,17 +14,20 @@
 
 include(InstallRequiredSystemLibraries)
 
+file(READ "${CMAKE_SOURCE_DIR}/VERSION" PROJECT_VERSION_RAW)
+string(STRIP "${PROJECT_VERSION_RAW}" PROJECT_VERSION_FULL)
+
 # installer rules. 
 set(CPACK_PACKAGE_NAME "Holovibes")
 set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "Holoflow - Digital Holography Software")
 set(CPACK_PACKAGE_VENDOR "Holoflow Developers")
-set(CPACK_PACKAGE_VERSION ${PROJECT_VERSION})
+set(CPACK_PACKAGE_VERSION ${PROJECT_VERSION_FULL})
 set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON)
 
 set(CPACK_NSIS_MUI_ICON "${CMAKE_CURRENT_SOURCE_DIR}/resources/holovibes/assets/holovibes_logo.ico")
-set(CPACK_PACKAGE_INSTALL_DIRECTORY "${PROJECT_VERSION}")
+set(CPACK_PACKAGE_INSTALL_DIRECTORY "${PROJECT_VERSION_FULL}")
 set(CPACK_NSIS_INSTALLED_ICON_NAME "${CMAKE_CURRENT_SOURCE_DIR}/resources/holovibes/assets/holovibes_logo.ico")
-set(CPACK_NSIS_DISPLAY_NAME "Holoflow ${PROJECT_VERSION}")
+set(CPACK_NSIS_DISPLAY_NAME "Holovibes ${PROJECT_VERSION_FULL}")
 set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
 set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
 
@@ -35,14 +38,14 @@ include(CPackComponent)
 cpack_add_install_type(Full DISPLAY_NAME "Install Everything")
 
 cpack_add_component(binaries
-    DISPLAY_NAME "Holoflow Binaries"
+    DISPLAY_NAME "Holovibes Binaries"
     DESCRIPTION "This will install the main application."
     REQUIRED
     INSTALL_TYPES Full 
 )
 
 cpack_add_component(dependencies
-    DISPLAY_NAME "Holoflow Dependencies"
+    DISPLAY_NAME "Holovibes Dependencies"
     DESCRIPTION "This will install the required dependencies."
     REQUIRED
     INSTALL_TYPES Full 
@@ -57,11 +60,11 @@ else (CMAKE_CL_64)
 endif (CMAKE_CL_64)
 
 set(CPACK_NSIS_CREATE_ICONS_EXTRA "
-    CreateShortCut '$DESKTOP\\\\Holoflow ${PROJECT_VERSION}.lnk' '$INSTDIR\\\\bin\\\\holovibes.exe'
+    CreateShortCut '$DESKTOP\\\\Holovibes ${PROJECT_VERSION_FULL}.lnk' '$INSTDIR\\\\bin\\\\holovibes.exe'
 ")
 
 set(CPACK_NSIS_DELETE_ICONS_EXTRA "
-    Delete '$DESKTOP\\\\Holoflow ${PROJECT_VERSION}.lnk'
+    Delete '$DESKTOP\\\\Holovibes ${PROJECT_VERSION_FULL}.lnk'
 ")
 
 include(CPack)
